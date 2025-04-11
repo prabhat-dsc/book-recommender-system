71bd03fb0f73dbb3cc97fb3be5915ac386bf2f53
prabhat-dsc <prabhat.dsc48@gmail.com>
2025-04-11 13:44:33 +0530

    setup file updated

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+## edit below variables as per your requirements -
+REPO_NAME = "book-recommender-system"
+AUTHOR_USER_NAME = "Prabhat Kumar"
+SRC_REPO = "books_recommender"
+LIST_OF_REQUIREMENTS = []
+
+
+setup(
+    name=SRC_REPO,
+    version="0.0.1",
+    author="Prabhat Kumar",
+    description="A small local packages for ML based books recommendations",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url= "https://github.com/prabhat-dsc/book-recommender-system.git",
+    author_email="prabhat.dsc48@gmail.com",
+    packages=find_packages(),
+    license="MIT",
+    python_requires=">=3.7",
+    install_requires=LIST_OF_REQUIREMENTS
+)
